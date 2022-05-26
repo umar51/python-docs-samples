a09e9c40e29db3d41e7b10472c805b12a9692bd8
Leah E. Cole <6719667+leahecole@users.noreply.github.com>
2022-05-26 14:41:22 -0400

    Update simple.py

--- composer/workflows/simple.py
+++ composer/workflows/simple.py
@@ -34,7 +34,7 @@ default_dag_args = {
     # fixed point in time rather than dynamically, since it is evaluated every
     # time a DAG is parsed. See:
     # https://airflow.apache.org/faq.html#what-s-the-deal-with-start-date
-    'start_date': datetime.datetime(2018, 1, 1),
+    'start_date': datetime.datetime(2022, 5, 24),
 }
 
 # Define a DAG (directed acyclic graph) of tasks.
